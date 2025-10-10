e3dfdd94146acf9a9a2c4d14de97fcdfb4283590
Jonah Colestock <rcolest@clemson.edu>
2025-10-10 19:30:01 -0400

    Filled adminRoutes.js

--- backend/admin-service/routes/adminRoutes.js
+++ backend/admin-service/routes/adminRoutes.js
@@ -1,1 +1,7 @@
-//empty
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/adminController');
+
+router.post('/events', adminController.addEvent);
+
+module.exports = router;

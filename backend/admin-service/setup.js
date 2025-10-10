bf7d74f35723d8363aa17e612854ee75ed4b4b3c
Jonah Colestock <rcolest@clemson.edu>
2025-10-10 18:52:42 -0400

    Made admin service initialization

--- backend/admin-service/setup.js
+++ backend/admin-service/setup.js
@@ -1,1 +1,17 @@
-//empty
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, '../shared-db/database.sqlite');
+const initSQL = fs.readFileSync(path.resolve(__dirname, '../shared-db/init.sql'), 'utf8');
+
+const db = new sqlite3.Database(dbPath);
+
+db.exec(initSQL, (err) => {
+    if(err){
+        console.error('Error initializing database:', err.message);
+    }else{
+        console.log('Database initialized successfully.');
+    }
+    db.close();
+});

a12f7a75219f5bc7cfd2977fa47208de69aece3d
TigerC77777 <cmskell@clemson.edu>
2025-11-01 15:14:23 -0400

    Another test

--- frontend/src/components/Events.test.js
+++ frontend/src/components/Events.test.js
@@ -7,18 +7,32 @@ test('Site loads', async () => {
     // This line is needed to ensure the "loading" screen is gone
     await screen.findByText("Campus Events");
 
-    const chatbotButton = screen.getByText("🎤 Speak");
-    expect(chatbotButton).toBeVisible();
+    const speechButton = screen.getByText("🎤 Speak");
+    expect(speechButton).toBeVisible();
 });
 
 test('Confirmation', async () => {
-    const user = userEvent.setup();
     render(<Events />);
     await screen.findByText("Campus Events");
 
     const ticketOptions = await screen.findAllByText("Buy Ticket");
-    await user.click(ticketOptions[0]);
+    await userEvent.click(ticketOptions[0]);
     const confirmbutton = screen.getAllByText('Yes');
 
     expect(confirmbutton[0]).toBeVisible();
+});
+
+test('Chatbot function', async () => {
+    render(<Events />);
+    await screen.findByText("Campus Events");
+
+    const chatbotButton = screen.getByText("Try our chatbot!");
+    await userEvent.click(chatbotButton);
+    const chatbotText = screen.getByRole('textbox');
+    await userEvent.click(chatbotText);
+    await userEvent.keyboard('book basketball game');
+    await userEvent.click(screen.getByRole('button', {name: 'Submit'}));
+
+    const message = screen.getAllByText('Yes');
+    expect(message[0]).toBeVisible();
 });

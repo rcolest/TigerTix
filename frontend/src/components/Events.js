cf82d104c32d699c3fc67085203cef8100380c2b
Jonah Colestock <rcolest@clemson.edu>
2025-12-05 00:39:01 -0500

    Slightly less stupid but still dumb path?

--- frontend/src/components/Events.js
+++ frontend/src/components/Events.js
@@ -15,7 +15,7 @@ export default function Events() {
 
   const fetchEvents = async () => {
     try {
-      const res = await fetch(`${API}/api/client/api/events`, {
+      const res = await fetch(`${API}/client/api/events`, {
         credentials: "include"
       });
       const data = await res.json();
